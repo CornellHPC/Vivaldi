f38a8f3173748ed05ef300b187a435941bda4717
Matthew Rubino <mrr245@cornell.edu>
2024-09-16 09:54:35 -0700

    Add distributed gemm

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --time=00:01:00
+#SBATCH --nodes=4
+#SBATCH --time=00:00:10
 #SBATCH --constraint=cpu
 #SBATCH --qos=debug
 
 cd /global/homes/m/mrrubino/cpp/distributed-popcorn/build
 
-# Run 16 MPI processes with 8 physical cores per process (8*2=16 logical cores)
-srun --ntasks 16 --cpus-per-task 16 main
+# Run 4 MPI processes
+srun --ntasks 4 main
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "CombBLAS/CombBLAS.h"
+#include <cstdint>
 // Macro "Error" is defined in CombBLAS and SLATE. It is unused in
 // CombBLAS, so we undefine it here to prevent name collisions.
 #undef Error
@@ -8,6 +9,33 @@
 #include <iostream>
 #include <mpi.h>
 
+// Column-major indexing
+template <typename scalar_type>
+void random_matrix(int64_t m, int64_t n, scalar_type *A, int64_t lda) {
+  for (int64_t j = 0; j < n; ++j) {
+    for (int64_t i = 0; i < m; ++i) {
+      A[i + j * lda] = blas::make_scalar<scalar_type>(
+          rand() / double(RAND_MAX), rand() / double(RAND_MAX));
+    }
+  }
+}
+
+// Column-major indexing
+template <typename matrix_type> void random_matrix(matrix_type &A) {
+  for (int64_t j = 0; j < A.nt(); ++j) {
+    for (int64_t i = 0; i < A.mt(); ++i) {
+      if (A.tileIsLocal(i, j)) {
+        try {
+          auto T = A(i, j);
+          random_matrix(T.mb(), T.nb(), T.data(), T.stride());
+        } catch (...) {
+          // ignore missing tiles
+        }
+      }
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   int rank, nprocs;
 
@@ -15,6 +43,29 @@ int main(int argc, char *argv[]) {
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  float alpha = 1.0f, beta = 0.0f;
+  int64_t m = 16, n = 16, k = 16, nb = 2;
+
+  slate::Matrix<float> A(m, n, nb, 2, 2, MPI_COMM_WORLD);
+  slate::Matrix<float> B(m, n, nb, 2, 2, MPI_COMM_WORLD);
+  slate::Matrix<float> C(m, n, nb, 2, 2, MPI_COMM_WORLD);
+  A.insertLocalTiles();
+  B.insertLocalTiles();
+  C.insertLocalTiles();
+  random_matrix(A);
+  random_matrix(B);
+  random_matrix(C);
+
+  slate::gemm(alpha, A, B, beta, C);
+
+  MPI_Finalize();
+
+  std::cout << "Graceful exit. Yay!" << std::endl;
+  return 0;
+}
+
+/*
 
   MPI_File fh;
   MPI_Status status;
@@ -30,8 +81,4 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < num_chars; ++i)
     std::cout << buf[i];
   std::cout << std::endl;
-
-  MPI_Finalize();
-
-  return 0;
-}
+*/
