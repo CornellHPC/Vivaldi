88415ce9c20c30f4f6a047e6ce8f9e77111dfbd5
Matthew Rubino <mrr245@cornell.edu>
2024-09-22 10:01:12 -0700

    Initialize combblas dense matrix

--- src/utils/matrix.hh
+++ src/utils/matrix.hh
@@ -75,19 +75,52 @@ slate_point_mat_to_polynomial_kernel_mat(slate::Matrix<scalar_type> M,
 }
 
 template <typename scalar_type>
+scalar_type get_slate_mat_value(slate::Matrix<scalar_type> M, int64_t ii,
+                                int64_t jj) {
+  int64_t mt = M.mt(), nt = M.nt();
+  int64_t i = ii / mt; // Tile row index
+  int64_t j = jj / nt; // Tile column index
+  ii -= i * mt;        // Element row index
+  jj -= j * nt;        // Element column index
+
+  slate::Tile<scalar_type> T = M(i, j);
+  scalar_type v = T(ii, jj);
+  return v;
+}
+
+template <typename scalar_type>
 combblas::DnParMat<int64_t, scalar_type>
 slate_mat_to_combblas_dpm(slate::Matrix<scalar_type> M) {
   slate::GridOrder order;
   int nprow, npcol, myrow, mycol;
   M.gridinfo(&order, &nprow, &npcol, &myrow, &mycol);
 
+  int wholerank;
+  MPI_Comm_rank(M.mpiComm(), &wholerank);
+
   std::shared_ptr<combblas::CommGrid> grid =
       std::make_shared<combblas::CommGrid>(M.mpiComm(), nprow, npcol);
   combblas::DnParMat<int64_t, scalar_type> D(grid, M.m(), M.n(),
                                              (scalar_type)0);
 
-  // TODO: Copy data from M to D
-  D.setarr(0, 1);
+  int rowrank = grid->GetRankInProcRow(wholerank);
+  int colrank = grid->GetRankInProcCol(wholerank);
+
+  int64_t rows_per_proc = D.getgnrow() / grid->GetGridRows();
+  int64_t ii_s = rowrank * rows_per_proc;
+  int64_t ii_e = ii_s + D.getnrow();
+
+  int64_t cols_per_proc = D.getgncol() / grid->GetGridCols();
+  int64_t jj_s = colrank * cols_per_proc;
+  int64_t jj_e = jj_s + D.getncol();
+
+  int x = 0;
+  for (int ii = ii_s; ii < ii_e; ++ii) {
+    for (int jj = jj_s; jj < jj_e; ++jj) {
+      D.setarr(x++, get_slate_mat_value(M, ii, jj));
+    }
+  }
+
   return D;
 }
 
