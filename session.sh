84303a1605b087372203235c6073c45cc4283bbf
Andrew Chang <andrewch@x1000c2s1b0n1h0.chn.perlmutter.nersc.gov>
2024-12-01 23:16:57 -0800

    reorg file structs andd a makefile for convenience

--- session.sh
+++ session.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-salloc --nodes 4 --qos interactive --time 01:00:00 --constraint gpu --gpus 16 --account m4341
+salloc --nodes 4 --qos interactive --time 03:00:00 --constraint gpu --gpus 16 --account m4341
