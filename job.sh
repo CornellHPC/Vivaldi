387c307c44922b33d794a083e825863936f65689
Matthew Rubino <mrr245@cornell.edu>
2024-12-08 10:36:49 -0800

    Add nsight profiling

--- job.sh
+++ job.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 #SBATCH --nodes=4
 #SBATCH --gpus=16
-#SBATCH --time=00:00:10
+#SBATCH --time=00:10:00
 #SBATCH --constraint=gpu
 #SBATCH --qos=debug
 #SBATCH --account=m4341
 #SBATCH --output=out/%j
 
 export DVS_MAXNODES=1__
-export MPICH_GPU_SUPPORT_ENABLED=1
-export SLATE_GPU_AWARE_MPI=1
+# export MPICH_GPU_SUPPORT_ENABLED=1
+# export SLATE_GPU_AWARE_MPI=1
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
 
 EXE_PATH=build/main
-CLUSTERS=2
+DATA="$PWD/data/rand 32000 64"
+CLUSTERS=128
 
-# Run 4 MPI processes
-srun --ntasks 4 --gpus 16 $EXE_PATH data/test 4 4 $CLUSTERS
-# srun --ntasks 4 --gpus 16 $EXE_PATH data/test_new 8 8 $CLUSTERS
+# Run 16 MPI processes
+srun --nodes=4 --ntasks=16 --cpus-per-task=32 --cpu-bind=cores --gpus=16 --gpu-bind=single:1 \
+  nsys profile --stats=true -t cuda,mpi -o benchmark/report \
+  $EXE_PATH $DATA $CLUSTERS
+
