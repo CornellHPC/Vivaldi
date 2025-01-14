45b14fa9f9a5b2a40e8d7ba711099360e6b7ffad
Matthew Rubino <mrr245@cornell.edu>
2025-01-14 14:41:11 -0800

    Add comments to sparse and c headers

--- src/cpop/compute_c.hh
+++ src/cpop/compute_c.hh
@@ -6,6 +6,15 @@
 
 namespace cpop {
 
+/**
+ * @brief Computes the c norm vector
+ *
+ * @param handle The cuSPARSE handle
+ * @param V The assignment matrix V
+ * @param ET The transposed point-centroid product matrix ET
+ * @param comm The MPI communicator used to distribute ET
+ * @return A cuSPARSE descriptor for the resulting dense vector
+ */
 cusparseDnVecDescr_t compute_c(cusparseHandle_t& handle, cusparseSpMatDescr_t V,
                                cusparseDnMatDescr_t ET, MPI_Comm comm);
 
--- src/cpop/compute_sparse.hh
+++ src/cpop/compute_sparse.hh
@@ -6,9 +6,26 @@
 
 namespace cpop {
 
+/**
+ * @brief Generates the V matrix using a round-robin assignment
+ *
+ * @param handle The cuSPARSE handle
+ * @param m The number of points
+ * @param k The number of clusters
+ * @param comm The MPI communicator used to distribute the matrix
+ * @return A cuSPARSE descriptor for the resulting sparse matrix
+ */
 cusparseSpMatDescr_t initialize_v(cusparseHandle_t& handle, int m, int k,
                                   MPI_Comm comm);
 
+/**
+ * @brief Multiplies a sparse matrix by a dense matrix
+ *
+ * @param handle The cuSPARSE handle
+ * @param V The sparse matrix
+ * @param K The dense matrix
+ * @return a cuSPARSE descriptor for the resulting dense matrix
+ */
 cusparseDnMatDescr_t spmm(cusparseHandle_t& handle, cusparseSpMatDescr_t& V,
                           cusparseDnMatDescr_t& K);
 
