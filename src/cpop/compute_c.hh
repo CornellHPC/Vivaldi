f1d53b2df0bc6b5be6edeb3600fb6336688c5d04
nakuliyer <nakulpiyer@gmail.com>
2025-01-26 12:53:34 -0800

    Docs

--- src/cpop/compute_c.hh
+++ src/cpop/compute_c.hh
@@ -7,16 +7,31 @@
 namespace cpop {
 
 /**
- * @brief Initializes the ET matrix before the SpMV routine
+ * @brief Initializes the c vector
  * 
  * @param k The number of clusters
- * @param c A cuSPARSE descriptor for the resulting dense vector
+ * @param c A cuSPARSE descriptor which will become a k-size vector
  * @return int 
  */
 int init_c(int k, cusparseDnVecDescr_t* c);
 
+/**
+ * @brief Initializes the z vector
+ * 
+ * @param t The tile size/width of K and ET
+ * @param z A cuSPARSE descriptor which will become a t-size vector
+ * @return int 
+ */
 int init_z(int t, cusparseDnVecDescr_t* z);
 
+/**
+ * @brief Computes z based on the local V matrix and ET (i.e. using the masking strategy)
+ * 
+ * @param lV The local CSC assignment matrix V
+ * @param ET The local partition of the ET dense matrix
+ * @param z Resulting z dense vector
+ * @return int 
+ */
 int compute_z(cusparseSpMatDescr_t& lV, cusparseDnMatDescr_t& ET,
               cusparseDnVecDescr_t& z);
 
