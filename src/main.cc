a71f0ded9cbf778e443ae4ff4c5d9c92f8b36cf5
Matthew Rubino <mrr245@cornell.edu>
2025-02-04 06:07:53 -0800

    Disable cuda aware mpi

--- src/main.cc
+++ src/main.cc
@@ -32,7 +32,7 @@ int cluster(char* path, int m, int n, int k, MPI_Comm comm) {
 
   /** INITIALIZE GPU */
   wake_gpus(rank);
-  slate::gpu_aware_mpi(true);
+  slate::gpu_aware_mpi(false);
   cusparseHandle_t handle;
   cusparseCreate(&handle);
 
