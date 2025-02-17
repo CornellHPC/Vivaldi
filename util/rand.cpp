7efb7d590e88d5613b9e8e75ab3043158b162cd5
Matthew Rubino <mrr245@cornell.edu>
2025-02-17 12:38:03 -0800

    Increase random size

--- util/rand.cpp
+++ util/rand.cpp
@@ -10,7 +10,7 @@ int main() {
 
   std::ofstream out;
   out.open("rand", std::ios::out | std::ios::binary);
-  for (int i = 0; i < 96000 * 64; ++i) {
+  for (int i = 0; i < 256000 * 64; ++i) {
     float p = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
     float val = (max - min) * p + min;
     out.write(reinterpret_cast<const char*>(&val), sizeof(float));
